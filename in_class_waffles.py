82847e7c56ccdd64784a2444a6f6f9997593b27d
Oliverflo61 <oliverflores6082@gmail.com>
2026-02-18 18:09:19 +0000

    completed in class assignment

---
+++ in_class_waffles.py
@@ -0,0 +1,50 @@
+In class asignment feb 18 2026 oliver flores claudia  leon
+
+# 1. Print a welcome message
+print("Hello, welcome to our codespace!")
+
+# 2. Create a variable with your name
+name = "Oliver"
+
+# 3. Print the name
+print(name)
+
+# 4. Create a variable with your age
+age = 61
+
+# 5. Print the age
+print()
+# 6. Ask the user for their favorite color
+print("What is your favorite color?")
+
+
+# 7. Print a sentence using the color
+print("Your favorite color is purple.")
+
+# 8. Create two number variables
+num1=17
+num2=21
+
+# 9. Add the numbers
+sum_nums= (num1+2)
+
+# 10. Print the result
+print (sum_nums)
+# 11. Check if age is greater than 18
+if age>18:
+# 12. Print message if true
+    print("You are older than 18 year old!")
+# 13. Otherwise print another message
+else:
+# 14. Print message if false
+   print("You are younger than 18 years old")
+
+# 15. Print a goodbye message
+print("Bye sucka!")
+
+
+
+
+
+# // Website we used : https://www.dummies.com/article/technology/programming-web-design/python/beginning-programming-python-dummies-cheat-sheet-207635/
+# https://www.w3schools.com/python/python_getstarted.asp
